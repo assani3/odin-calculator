a1c37940c9f0b6df07433d7c6aafe841840dd325
assani3 <assanishabani51@gmail.com>
2026-01-18 08:06:45 +0200

    operator functions

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-function add(a, b){
-    return a + b;
+function add(...num){
+    let sum = 0;
+    for(let i = 0; i < num.length; i++){
+       sum += num[i]; 
+    }
+    return sum;
 }
 
-function subtract(a, b){
-    return a - b;
+function subtract(...num){
+    let result = num[0];
+    for(let i = 1; i < num.length; i++){
+        result -= num[i];
+    }
+    return result;
 }
 
-function multiply(a, b){
-    return a * b;
+function multiply(...num){
+    let factor = num[0];
+    for(let i = 1; i < num.length; i++){
+        factor *= num[i];
+    }
+    return factor;
 }
 
-function divide(a, b){
-    return a / b;
+function divide(...num){
+    let division = num[0];
+    for(let i = 1; i < num.length; i++){
+        division /= num[i];
+    }
+    return division;
 }
+
+console.log(add(2, 2, 2, 2));
+console.log(subtract(5, 2, 1, 1));
+console.log(multiply(2, 3, 2, 2));
+console.log(divide(24, 2, 2, 2));
+console.log("hello world");
