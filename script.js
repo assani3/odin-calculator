8dd2e2f856888f0d7f598b5b34070b8acdbd1927
assani3 <assanishabani51@gmail.com>
2026-01-27 08:03:32 +0200

    finished work

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-//three variables, one for each part of the operation
-let operator;
-let num1;
-let num2;
+//variables, one for each part of the operation
+let operator = null;
+let num1 = null;
+let num2 = null;
+let userNumbers1 = [];
+let result;
 
+//add, subtract, multiply and divide function
 function add(...num){
     let sum = 0;
     for(let i = 0; i < num.length; i++){
@@ -35,62 +38,107 @@ function divide(...num){
     return division;
 }
 
-console.log(add(2, 2, 2, 2));
-console.log(subtract(5, 2, 1, 1));
-console.log(multiply(2, 3, 2, 2));
-console.log(divide(24, 2, 2, 2));
-console.log("hello world");
-
+//fucntion dealing with the calculations
 function operate(number1, operator1, number2){
 
+    // Convert strings to real numbers here
+    const n1 = Number(number1);
+    const n2 = Number(number2);
+    
+
     if(operator1 === "+"){
-        return add(number1, number2);
-
+        return add(n1, n2);
     } else if(operator1 === "-"){
-        return subtract(number1, number2);
-
-    } else if(operator1 === "*"){
-        return multiply(number1, number2);
-
+        return subtract(n1, n2);
+    } else if(operator1 === "x"){
+        return multiply(n1, n2);
     } else if(operator1 === "/"){
-        return divide(number1, number2);
+        return divide(n1, n2);
     }
-
 }
 
-//Next session work on these
+
+
+
+
+//Fucntion dealing with num1 and num2
 function updateNumber1(num){
-num1 = num;
-console.log(num1)
+
+const number = Number(num)
+
+if(!isNaN(number)){
+    userNumbers1.push(number)
+    let glued = userNumbers1.join('');
+        // If no operator has been picked, we are still building num1
+        if (operator === null) {
+            num1 = glued;
+    } else {
+            // If an operator exists, we are building num2
+            num2 = glued;
+    }
+}
 }
 
-function updateDisplay(elm){
-    document.querySelector(".display").textContent = elm;
 
+//RECEIVING THE OPERATOR
+function updateOperator(receivedOperator){
+    operator = receivedOperator;
+    // START AFRESH we clear the temporary array for the second number
+    userNumbers1 = []; 
+}
+
+
+function updateDisplay(){
+
+//Displays '' only if contentToShow is strictly null or undefined.
+let displayNum1 = num1 ?? ''; 
+let displayOperator = operator ?? ''; 
+let displayNum2 = num2 ?? ''; 
+
+document.querySelector(".display").textContent = displayNum1 + displayOperator + displayNum2; 
 }
 updateDisplay()
 
-//Event delegation for our buttons
 
+
+
+//Event delegation for our buttons
 const div = document.querySelector(".buttons");
 
-//console.log("div: ", div)
-div.addEventListener('click', event =>{
+
+div.addEventListener('click', event => {
     const target = event.target;
+    if (target.tagName !== 'BUTTON') return;
 
-    if(target.tagName === 'BUTTON'){
-       // console.log(target.innerText);
-        updateNumber1(target.innerText);
-        updateDisplay(target.innerText);
+    const value = target.innerText;
+
+    if (!isNaN(value)) {
+        // 1. Update the numbers
+        updateNumber1(value);
+        console.log("num1:", num1, "num2:", num2);
+    } else if (value === "AC") {
+        // 2. Handle Clear 'AC'
+        num1 = null;
+        num2 = null;
+        operator = null;
+        userNumbers1 = []; // Clears the array too
+    } else if (value === "=") {
+        // 3. Handle Calculation
+        if (num1 !== null && num2 !== null && operator !== null) {
+             result = operate(num1, operator, num2);
+            console.log("Result:", result);
+            
+            // IMPORTANT: Usually you want the result to become the new num1
+            num1 = result;
+            num2 = null;
+            operator = null;
+            userNumbers1 = [result]; // Reset helper array with the result
+        }
+    } else {
+        // 4. Handle Operators (+, -, *, /)
+        updateOperator(value);
+        console.log("Operator:", operator);
     }
+
+    updateDisplay();
 });
-
-//we are getting somehwere so now when we click the numbers
-//they can go to the update function but it's not only numbers that go
-// it is all the buttons
-
-//we need to find a way to only accept numbers or only send 
-//numbers
-
-//try to make the display work as well it must show numbers being clickes
-//and also show the answer if possible
